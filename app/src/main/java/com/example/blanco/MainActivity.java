af6a4086407103e0bfb835f2cdc1b1a145460398
Alex <ezyah3@nottingham.ac.uk>
2018-08-16 18:01:10 +0200

    Added new activity - Login - and linked together

--- app/src/main/java/com/example/blanco/MainActivity.java
+++ app/src/main/java/com/example/blanco/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.blanco;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -20,4 +22,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
     }
+
+    public void logIn(View v) {
+        startActivity(new Intent(this,LoginActivity.class));
+    }
+
+
 }
