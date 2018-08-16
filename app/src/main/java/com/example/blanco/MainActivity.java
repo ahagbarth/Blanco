ff9eb856df00347d52f41ef4377b0c7e548171a8
Alex <ezyah3@nottingham.ac.uk>
2018-08-16 15:51:36 +0200

    Initial commit - start of app

---
+++ app/src/main/java/com/example/blanco/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.blanco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Hide the Title Bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+    }
+}
